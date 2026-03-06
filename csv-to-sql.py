8ac3c9dfdfa57adfa6eb6db59dada717e32fd70d
Autism Origins Lab <aylew2002@gmail.com>
2026-03-06 09:53:12 -0500

    fixed bug, added ability to set header types for database and ability to name database, added verification that header types and name are set

--- csv-to-sql.py
+++ csv-to-sql.py
@@ -4,10 +4,16 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 # list of datatypes
-datatypes = ["Integer", "Decimal", "Date/Time", "Text", "Boolean"]
+datatypes = ["Boolean", "Date/Time", "Decimal", "Integer", "Text"]
 
 # stores CSV filename to open
 filename = "None"
+
+# stores dataframe
+dataframe = pd.DataFrame()
+
+# stores header types
+headertypes = []
 
 # function to choose CSV file
 def choose_csv_file():
@@ -37,35 +43,68 @@ def upload():
     root.destroy()
 
     # read CSV or Excel file into pandas dataframe
-    dataframe = pd.DataFrame()
+    global dataframe
     if filetype == "csv":
         dataframe = pd.read_csv(filename)
     elif filetype == "excel":
         dataframe = pd.read_excel(filename)
 
     # create new GUI window
+    global processor
     processor = tk.Tk()
     processor.minsize(400, 200)
     processor.title("Choose Data Types")
 
     # stores labels and options
+    global headertypes
     labels = []
-    options = []
     optionmenus = []
 
     # list headers for new GUI window
     for i in range(len(dataframe.columns)):
         # create labels & options
         labels += [tk.Label(processor, text=dataframe.columns[i], justify="left")]
-        labels[i].grid(row=i, column=0, sticky="W")
-        options += [tk.StringVar()]
-        optionmenus += [tk.OptionMenu(processor, options[i], "Choose an option...", *datatypes)]
-        optionmenus[i].grid(processor=i, column=1, sticky="W")
+        headertypes += [tk.StringVar()]
+        headertypes[i].set(" ")
+        optionmenus += [tk.OptionMenu(processor, headertypes[i], None, *datatypes)]
 
         # display labels & options
-        labels[i].pack()
-        optionmenus[i].pack()
+        labels[i].grid(row=i, column=0, sticky="W")
+        optionmenus[i].grid(row=i, column=1, sticky="W")
+    
+    # create variable to store name
+    global name
+    name = tk.StringVar()
+    name.set(" ")
 
+    # name database
+    label = tk.Label(processor, text="Database Name: ", justify="left")
+    namefield = tk.Entry(processor, textvariable=name)
+    label.grid(row=len(dataframe.columns), column=0, sticky="W")
+    namefield.grid(row=len(dataframe.columns), column=1, sticky="W")
+
+    # display button
+    submitbutton = tk.Button(processor, text="Create Database", command=create_db)
+    submitbutton.grid(row=len(dataframe.columns)+1, column=0, sticky="W")
+
+# command to create database
+def create_db():
+    # verify that all the columns have been given a type
+    for type in headertypes:
+        if type.get() == " ":
+            print("failed")
+            messagebox.showerror("Error: Type Not Specified", "All columns must have its data type specified.")
+            return
+    
+    # verify that the database has a name
+    if name.get() == " ":
+        print("failed")
+        messagebox.showerror("Error: Name Not Specified", "A name must be specified for the database.")
+        return
+        
+    # create sql database
+    print("created database {0}".format(name.get()))
+    processor.destroy()
 
 # create GUI window
 root = tk.Tk()
